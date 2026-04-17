49521ba17f9696c17f0cd8e572488eeb8403f4ba
Suyash Sharma <Sharmasuyash198@gmail.com>
2026-04-17 21:17:55 +0530

    add basic frontend

---
+++ backend/models/Log.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const logSchema = new mongoose.Schema({
+  userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+  },
+  action: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Log', logSchema);
